70f0063cb418f212354358fc71be3b2af80667b7
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-03-13 16:43:33 +0200

    First working version of indent converter. No comments of error checks yet.

--- indconvert.py
+++ indconvert.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
-def indentation_converter(filename,old_ind=2,new_ind=4):
-    pass
+def indentation_converter(filename,ind_char=' ',old_ind=2,new_ind=4):
+    with open(filename,'r') as f:
+        for line in f:
+            strippedline = line.lstrip(ind_char)
+            indcount = len(line) - len(strippedline)
+            newline = ind_char*(indcount/old_ind*new_ind) + strippedline
+            with open (filename+'_new','a') as of:
+                of.write(newline)
 
 def main():
-    pass
+    filename = 'testfile.txt'
+    indentation_converter(filename)
 
 if __name__=='__main__':
     main()
