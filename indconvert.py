6fc4ac3d4aa4cb2042c72fa1615faa54e4f7ed7a
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-03-17 13:33:12 +0200

    Changed the behavior to return individual lines, not writing to file.

--- indconvert.py
+++ indconvert.py
@@ -5,32 +5,23 @@ import sys
 # Add argument parsing.
 # Add some checks (e.g., for when the file has an unexpected # of indents).
 
-def indentation_converter(filename,ind_char=' ',old_ind=2,new_ind=4):
+def indentation_converter(line,ind_char=' ',old_ind=2,new_ind=4):
     """Converts indentation from one character and length to something else.
     Arguments:
-    filename -- The file name to be changed. The original file will not be touched
+    line -- The line to be changed
     ind_char -- What character is used for indentation
     old_ind -- Old indentation (number of spaces, tabs etc.)
     new_ind -- New indent count
     Output:
-    Returns nothing. Writes changes to [filename_new] file
+    newline -- The modified line
     """
-    # Check for cli args. This will be changed to argparse
-    if len(sys.argv)>1:
-        filename = sys.argv[1]
-    # Open the file
-    with open(filename,'r') as f:
-        # Process line by line
-        for line in f:
-            # Strip the indent characters
-            strippedline = line.lstrip(ind_char)
-            # Measure how many indents there were originally in the line
-            indcount = len(line) - len(strippedline)
-            # Create the new line with the desired indent count
-            newline = ind_char*(indcount/old_ind*new_ind) + strippedline
-            # Write to output file
-            with open (filename+'_new','a') as of:
-                of.write(newline)
+    # Strip the indent characters
+    strippedline = line.lstrip(ind_char)
+    # Measure how many indents there were originally in the line
+    indcount = len(line) - len(strippedline)
+    # Create the new line with the desired indent count
+    newline = ind_char*(indcount/old_ind*new_ind) + strippedline
+    return newline
 
 def main():
     pass
