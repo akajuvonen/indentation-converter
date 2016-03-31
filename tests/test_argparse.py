6c7df3b5ecb5684e6c6ff470abfa1e7a9a938d5a
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-03-31 12:31:50 +0300

    Added placeholder and comments for a new test.

--- tests/test_argparse.py
+++ tests/test_argparse.py
@@ -10,10 +10,17 @@ from indconvert import parse_args
 class TestArgParse(unittest.TestCase):
     """Tests for argument parsing."""
     def test_arg_parse(self):
-        """Test that argument parsing works normally."""
+        """Test that argument parsing works normally"""
         args = ['-f','filename','-i',' ','-o','2','-n','4']
         fn,i,o,n = parse_args(args)
         self.assertEqual(fn,args[1])
         self.assertEqual(i,args[3])
         self.assertEqual(o,int(args[5]))
         self.assertEqual(n,int(args[7]))
+
+    def test_arg_missing(self):
+        """Test missing argument"""
+        # This will done later, since when an argument is missing
+        # argparse just give sys.exit, which is problematic for
+        # testing and gives an error.
+        pass
