db0728728438052e98ce02bac61be463027ae7e6
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-10-09 10:58:59 +0300

    Changed code style to PEP8 standard

--- indconvert.py
+++ indconvert.py
@@ -2,11 +2,13 @@
 import sys
 import argparse
 
+
 class IndentationException(Exception):
     """An exception that is raised when the indentation count doesn't match."""
     pass
 
-def indentation_converter(line,ind_char=' ',old_ind=2,new_ind=4):
+
+def indentation_converter(line, ind_char=' ', old_ind=2, new_ind=4):
     """Converts indentation from one character and length to something else.
     Arguments:
     line -- The line to be changed
@@ -21,11 +23,13 @@ def indentation_converter(line,ind_char=' ',old_ind=2,new_ind=4):
     # Measure how many indents there were originally in the line
     indcount = len(line) - len(strippedline)
     # If the indentation count doesn't match, raise an exception
-    if indcount%old_ind > 0:
-        raise IndentationException('The number of indentation not divisible by indentation count: ' + str(old_ind))
+    if indcount % old_ind > 0:
+        raise IndentationException(
+            'The number of indentation not divisible by indentation count: ' + str(old_ind))
     # Create the new line with the desired indent count
-    newline = ind_char*(indcount/old_ind*new_ind) + strippedline
+    newline = ind_char * (indcount / old_ind * new_ind) + strippedline
     return newline
+
 
 def parse_args(args):
     """Argument parser for indentation converter.
@@ -38,15 +42,20 @@ def parse_args(args):
     newind -- New indentation count (int)
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f','--filename',type=str,help='The filename of the file that needs its indentation changed',required=True)
-    parser.add_argument('-i','--indchar',type=str,help='The character used for indentation',required=True)
-    parser.add_argument('-o','--oldind',type=int,help='The existing number of indentation characters used (for one indentation)',required=True)
-    parser.add_argument('-n','--newind',type=int,help='The new wanted number of indentation characters',required=True)
+    parser.add_argument('-f', '--filename', type=str,
+                        help='The filename of the file that needs its indentation changed', required=True)
+    parser.add_argument('-i', '--indchar', type=str,
+                        help='The character used for indentation', required=True)
+    parser.add_argument('-o', '--oldind', type=int,
+                        help='The existing number of indentation characters used (for one indentation)', required=True)
+    parser.add_argument('-n', '--newind', type=int,
+                        help='The new wanted number of indentation characters', required=True)
     arguments = parser.parse_args(args)
     v = vars(arguments)
-    return v['filename'],v['indchar'],v['oldind'],v['newind']
+    return v['filename'], v['indchar'], v['oldind'], v['newind']
 
-def read_write(filename,indchar,oldind,newind):
+
+def read_write(filename, indchar, oldind, newind):
     """Reads the specified file and writes the lines with new indentation
     to the new file.
     Arguments:
@@ -56,17 +65,19 @@ def read_write(filename,indchar,oldind,newind):
     newind -- New indentation
     """
     # Open and read line by line
-    with open(filename,'r') as infile:
+    with open(filename, 'r') as infile:
         for line in infile:
-            with open(filename+'_out','a') as outfile:
+            with open(filename + '_out', 'a') as outfile:
                 # Write the result line into output file
-                outfile.write(indentation_converter(line,indchar,oldind,newind))
+                outfile.write(indentation_converter(
+                    line, indchar, oldind, newind))
+
 
 def main():
     # Parse the arguments
-    filename,indchar,oldind,newind = parse_args(sys.argv[1:])
+    filename, indchar, oldind, newind = parse_args(sys.argv[1:])
     # Read the infile and write to a new file
-    read_write(filename,indchar,oldind,newind)
+    read_write(filename, indchar, oldind, newind)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
--- tests/test_argparse.py
+++ tests/test_argparse.py
@@ -7,16 +7,18 @@ from indconvert import parse_args
 # shows a message and exits when anything goes wrong. It's not very
 # test friendly. Perhaps these will be added later.
 
+
 class TestArgParse(unittest.TestCase):
     """Tests for argument parsing."""
+
     def test_arg_parse(self):
         """Test that argument parsing works normally"""
-        args = ['-f','filename','-i',' ','-o','2','-n','4']
-        fn,i,o,n = parse_args(args)
-        self.assertEqual(fn,args[1])
-        self.assertEqual(i,args[3])
-        self.assertEqual(o,int(args[5]))
-        self.assertEqual(n,int(args[7]))
+        args = ['-f', 'filename', '-i', ' ', '-o', '2', '-n', '4']
+        fn, i, o, n = parse_args(args)
+        self.assertEqual(fn, args[1])
+        self.assertEqual(i, args[3])
+        self.assertEqual(o, int(args[5]))
+        self.assertEqual(n, int(args[7]))
 
     def test_arg_missing(self):
         """Test missing argument"""
